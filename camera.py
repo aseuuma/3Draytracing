d8c736f52980fc13f7ffc3018da0059981f8d4d0
GUERDOUH asma <121774615+aseuuma@users.noreply.github.com>
2024-11-05 21:01:00 +0100

    Update camera.py

--- camera.py
+++ camera.py
@@ -3,10 +3,30 @@ import numpy as np
 
 
 class camera :
-    def __init__(self, position, target):
-        self.position = np.array(position)
-        self.target = np.array(target)
-    def plan_de_projection ():
+    def __init__(self, position, df ,alpha,beta):
+        self.position = np.array(position)  
+        self.focal_de_camera = df
+        self.alpha = alpha
+        self.beta = beta
 
-        return 
+
+
+        self.intrinsic_matrix = self.calculer_de_K()
+       
+
+    def calculer_de_K(self):
+        
+        fALPHA = self.focal_de_camera * self.alpha
+        fBETA = self.focal_de_camera * self.beta
+        u0, v0 = self.principal_point
+        
+        K = np.array([
+            [fALPHA, 0, u0],
+            [0, fBETA, v0],
+            [0, 0, 1]
+        ])
+        return K
+    
+
+       
     
