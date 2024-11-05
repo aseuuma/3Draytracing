ba1f2aa6930a395dfca933898c8f76bbfd15f4c1
GUERDOUH asma <121774615+aseuuma@users.noreply.github.com>
2024-11-05 23:23:48 +0100

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,42 +1,48 @@
 import pygame
 import numpy as np
 
-
-class camera :
-    def __init__(self, position, df ,alpha,beta,translation_vector,matrice_de_rotation , mash):
-        self.position = np.array(position)  
-        self.focal_de_camera = df
+class Camera : 
+    def __init__(self, focal_length, alpha, beta, principal_point, rotation_matrix, translation_vector, mesh):
+        
+        self.focal_length = focal_length #focalde de camera
         self.alpha = alpha
         self.beta = beta
-        self.translation_vector = translation_vector
-        self.matrice_de_rotation=matrice_de_rotation
-
-        self.matrice_des_paramètres_intrinsèques= self.calculer_de_K()
-        self.matrice_des_paramètres_extrinsiques = self.calculer_de_RS()
-        self.mash = mash
-    def calculer_de_K(self):
+        self.principal_point = np.array(principal_point)
         
-        fALPHA = self.focal_de_camera * self.alpha
-        fBETA = self.focal_de_camera * self.beta
-        u0, v0 = self.position
+       
+        self.rotation_matrix = np.array(rotation_matrix)
+        self.translation_vector = np.array(translation_vector).reshape(-1, 1)
+        
+        
+        self.mesh = mesh
+        
+       
+        self.intrinsic_matrix = self.compute_intrinsic_matrix()
+        self.extrinsic_matrix = self.compute_extrinsic_matrix()
+        self.projection_matrix = self.compute_projection_matrix()
+
+    def compute_intrinsic_matrix(self):
+        falpha = self.focal_length * self.alpha
+        fbeta = self.focal_length * self.beta
+        u0, v0 = self.principal_point
         
         K = np.array([
-            [fALPHA, 0, u0],
-            [0, fBETA, v0],
+            [falpha, 0, u0],
+            [0, fbeta, v0],
             [0, 0, 1]
         ])
         return K
-    def calculer_de_RS(self):
-        RS =np.hstack((self.matrice_de_rotation, self.translation_vector))
-        return RS
-    def matrice_de_projection (self):
-        p=np.dot(self.matrice_des_paramètres_intrinsèques, self.matrice_des_paramètres_extrinsiques)
- 
-        return p
+
+    def compute_extrinsic_matrix(self):
+        return np.hstack((self.rotation_matrix, self.translation_vector))
+
+    def compute_projection_matrix(self):
+        return np.dot(self.intrinsic_matrix, self.extrinsic_matrix)
+
+   
     def project_points_3d(self):
       
-        points = np.array(self.mash.vertices)
+        points = np.array(self.mesh.vertices)
         points_3d = np.hstack((points, np.ones((points.shape[0], 1))))
         points_2d= np.dot(self.projection_matrix, points_3d.T).T
-        return points_2d[:, :2] / points_2d[:, 2, np.newaxis] 
-    
+        return points_2d[:, :2] / points_2d[:, 2, np.newaxis]
