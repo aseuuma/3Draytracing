6e26b87a037f4b2a9205d360a72d99e79d4c9d83
GUERDOUH asma <121774615+aseuuma@users.noreply.github.com>
2024-11-05 09:05:40 +0100

    Update maincode.py

---
+++ maincode.py
@@ -0,0 +1,47 @@
+import pygame
+import numpy as np
+
+pygame.init()
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("3D ratracing")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pygame.quit()
