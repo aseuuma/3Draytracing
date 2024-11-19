34efeb051402228aee0b7412fae27d3ab7921249
GUERDOUH asma <121774615+aseuuma@users.noreply.github.com>
2024-11-19 14:28:16 +0100

    ray
    ray

--- sphere.py
+++ sphere.py
@@ -1,9 +1,11 @@
 import numpy as np
-
+from Ray import ray
 
 
 class Sphere:
-  def __init__(self, center, radius, color):
+  def __init__(self, center, radius, color , ray):
         self.center = np.array(center)
         self.radius = radius
         self.color = color
+        self.ray = ray
+  
