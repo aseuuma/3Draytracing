4212683389ec870f13bfb4785c6e681b26b74dcb
GUERDOUH asma <121774615+aseuuma@users.noreply.github.com>
2024-11-20 18:37:31 +0100

    intersection de ray avec sphere

--- sphere.py
+++ sphere.py
@@ -12,3 +12,21 @@ class Sphere:
         self.color = color
         self.ray = ray
   
+  def intersect(self, ray_origin, ray_direction):
+        oc = ray_origin - self.center
+        a = np.dot(ray_direction, ray_direction)
+        b = 2.0 * np.dot(oc, ray_direction)
+        c = np.dot(oc, oc) - self.radius ** 2
+        discriminant = b ** 2 - 4 * a * c
+
+        if discriminant < 0:
+            return None
+
+        t1 = (-b - np.sqrt(discriminant)) / (2 * a)
+        t2 = (-b + np.sqrt(discriminant)) / (2 * a)
+
+        if t1 > 0:
+            return t1
+        if t2 > 0:
+            return t2
+        return None
