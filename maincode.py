58ad9bfd16bf60cb809df05a608ab95a434602cd
GUERDOUH asma <121774615+aseuuma@users.noreply.github.com>
2024-11-20 18:58:58 +0100

    main update

--- maincode.py
+++ maincode.py
@@ -2,18 +2,23 @@ import pygame
 import numpy as np
 from  mesh import Mesh
 from camera import Camera
-
+from sphere import Sphere
 pygame.init()
-width, height = 800, 600
+width, height = 400, 400
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("3D Raytracing")
 
 
-file_path = 'CUBEE.obj'  
+file_path = 'eyeball.obj'  
 mesh_object = Mesh(file_path)
 mesh_object.open_3D_object()
 
-
+spheres = [
+    Sphere(center=[0, 0, -5], radius=2, color=(255, 0, 0)),   
+    Sphere(center=[2, 1, -6], radius=1, color=(0, 255, 0)), 
+    Sphere(center=[-2, -1, -4], radius=1, color=(0, 0, 255)),
+    Sphere(center=[-2,29 , -4], radius=2, color=(0, 255, 255))
+]
 camera = Camera(
     focal_length=2,
     alpha=400,
@@ -25,13 +30,39 @@ camera = Camera(
 )
 
 running = True
+clock = pygame.time.Clock()
+
+for y in range(screen_height):
+    for x in range(screen_width):
+        ray_origin, ray_direction = camera.generate_ray(x, y)
+
+        pixel_color = (0, 0, 0)  # Default background color (black)
+        intersection_point = None
+
+ 
+        for sphere in spheres:
+            t = sphere.intersect(ray_origin, ray_direction)
+            if t is not None:
+                intersection_point = ray_origin + t * ray_direction  # Calculate intersection point
+                pixel_color = sphere.color
+                break  # Once we find the intersection, no need to check further spheres
+
+       
+        screen.set_at((x, y), pixel_color)
+   
+        if pixel_color != (0, 0, 0):
+            print(f"Pixel ({x},{y}): Intersection at t={t}")
+
+    pygame.display.flip()
+
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-  
-    screen.fill((0, 0, 0))
+pygame.quit()
+
 
     projected_points = camera.project_avec_rotation()
 
