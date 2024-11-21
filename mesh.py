9bfcaddd3bd01c303f54f2701edcf2f9006a8e94
GUERDOUH asma <121774615+aseuuma@users.noreply.github.com>
2024-11-21 02:39:51 +0100

    Update mesh.py

--- mesh.py
+++ mesh.py
@@ -1,25 +1,29 @@
-import numpy as np
+import numpy as np 
+
 class Mesh:
-    def __init__(self, filename):
-        self.filename = filename
-        self.vertices = []
-        self.faces = []
-        self.VN = []
-        self.VT=[]
-    
-    def open_3D_object(self):
-        with open(self.filename) as file:
+    def __init__(self, file_path):
+        # Read and parse the .obj file to extract vertices
+        self.vertices , self.faces = self.open_3D_object(file_path)
+
+    def open_3D_object(self, file_path):
+        vertices = []
+        faces = []
+
+        with open ( file_path, 'r') as file:
             for line in file:
-                if line.startswith("v "):
-                    vertex = list(map(float, line.strip().split()[1:4]))
-                    self.vertices.append(vertex)
-                elif line.startswith("f "):
-                    face = line.strip().split()[1:] 
-                    vertex_indices = [int(v.split('/')[0]) - 1 for v in face] 
-                    self.faces.append(vertex_indices)
-                elif line.startswith("vt "):
-                    vertex = list(map(float, line.strip().split()[1:4]))
-                    self.VT.append(vertex)
-                elif line.startswith("vn "):
-                    vertex = list(map(float, line.strip().split()[1:4]))
-                    self.VN.append(vertex)
+                if line.startswith('v '):
+                    # Read vertex positions
+                    parts = line.strip().split()
+                    print(parts)
+                    vertices.append([float(parts[1]), float(parts[2]), float(parts[3])])
+                elif line.startswith('f '):
+                    # Read face definitions
+                    parts = line.strip().split()
+                    print(parts)
+                    face = [int(p.split('/')[0]) - 1 for p in parts[1:]]
+                    print(face)
+                    faces.append(face)
+
+        return np.array(vertices), faces
+
+  
