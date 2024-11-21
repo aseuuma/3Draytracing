ff9bf8a9450f17aa362084b9203e97f3e6aec635
GUERDOUH asma <121774615+aseuuma@users.noreply.github.com>
2024-11-21 02:40:15 +0100

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,22 +1,17 @@
-import pygame
 import numpy as np
 
-class Camera : 
-    def __init__(self, focal_length, alpha, beta, principal_point, rotation_matrix, translation_vector, mesh):
-        
-        self.focal_length = focal_length #focalde de camera
+class Camera:
+    def __init__(self, focal_length, alpha, beta, principal_point, rotation_matrix, translation_vector, mesh, width, height):
+        self.focal_length = focal_length
         self.alpha = alpha
         self.beta = beta
         self.principal_point = np.array(principal_point)
-        self.aspect_ratio = screen_width / screen_height
-       
         self.rotation_matrix = np.array(rotation_matrix)
         self.translation_vector = np.array(translation_vector).reshape(-1, 1)
-        
-        
         self.mesh = mesh
-        
-       
+        self.width = width
+        self.height = height
+
         self.intrinsic_matrix = self.compute_intrinsic_matrix()
         self.extrinsic_matrix = self.compute_extrinsic_matrix()
         self.projection_matrix = self.compute_projection_matrix()
@@ -24,8 +19,7 @@ class Camera :
     def compute_intrinsic_matrix(self):
         falpha = self.focal_length * self.alpha
         fbeta = self.focal_length * self.beta
-        u0, v0 = self.principal_point
-        
+        u0, v0 , F = self.principal_point
         K = np.array([
             [falpha, 0, u0],
             [0, fbeta, v0],
@@ -37,33 +31,11 @@ class Camera :
         return np.hstack((self.rotation_matrix, self.translation_vector))
 
     def compute_projection_matrix(self):
-        return np.dot(self.intrinsic_matrix, self.extrinsic_matrix)   
+        return np.dot(self.intrinsic_matrix, self.extrinsic_matrix)
 
-   
-    def project_avec_rotation(self):
-      
+
+    def project_points_3d(self):
         points = np.array(self.mesh.vertices)
         points_3d = np.hstack((points, np.ones((points.shape[0], 1))))
-        points_2d= np.dot(self.projection_matrix, points_3d.T).T
+        points_2d = np.dot(self.projection_matrix, points_3d.T).T
         return points_2d[:, :2] / points_2d[:, 2, np.newaxis]
-    def project_points_3dds(self):
-        points = np.array(self.mesh.vertices)
-
-        points[:, 2] += self.translation_vector[2] 
-        projected_points = points[:, :2] / points[:, 2, np.newaxis] #مقسمة على Z هنا
-
-        projected_points[:, 0] = projected_points[:, 0] *self.alpha *self.focal_length + self.principal_point[0]
-        projected_points[:, 1] = projected_points[:, 1] * self.beta *self.focal_length + self.principal_point[1]
-
-        return projected_points
-     def generate_ray(self, pixel_x, pixel_y):
-        # Convert pixel coordinates to normalized device coordinates
-        ndc_x = (pixel_x + 0.5) / self.screen_width * 2 - 1
-        ndc_y = 1 - (pixel_y + 0.5) / self.screen_height * 2
-        ndc_y /= self.aspect_ratio
-
-        # Ray direction in camera space
-        ray_direction = np.array([ndc_x, ndc_y, -self.focal_length])
-        ray_direction /= np.linalg.norm(ray_direction)
-        ray_origin = np.array([0, 0, 0])
-        return ray_origin, ray_direction
