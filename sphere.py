70ef2b0785ceeb0c29fe0abdc04392c58d1f0e90
aseuuma <asma02guerdouh@gmail.com>
2024-11-25 21:21:25 +0100

    Calculate surface normal

--- sphere.py
+++ sphere.py
@@ -1,8 +1,17 @@
 import numpy as np
-
+from camera import Camera
 class Sphere:
   def __init__(self, center, radius, color , ray):
         self.center = np.array(center)
         self.radius = radius
         self.color = color
         
+  def get_normal(self, point):
+        # Calculate surface normal
+        return (point - self.center) / np.linalg.norm(point - self.center)
+  def normal(self, ray_origin, ray_direction):
+        t = Camera.intersect(ray_origin, ray_direction)
+        if t is None:
+            return None  
+        intersection_point = ray_origin + t * ray_direction
+        return self.get_normal(intersection_point)
