02f10af771bf0bf9a624891a2ba6cff5d033ce3a
aseuuma <asma02guerdouh@gmail.com>
2024-11-25 21:30:17 +0100

    model lambert lightning

--- light.py
+++ light.py
@@ -3,3 +3,5 @@ class Light:
     def __init__(self, position, intensity):
         self.position = np.array(position)
         self.intensity = intensity
+    def model_lambert_lightning(normal, light_direction, light_intensity):
+        return max(np.dot(normal, light_direction), 0) * light_intensity
