71c2636fd202d17f32f9ae062ca84475b6979c78
aseuuma <asma02guerdouh@gmail.com>
2024-12-02 19:36:11 +0100

    medele phong

--- light.py
+++ light.py
@@ -5,3 +5,8 @@ class Light:
         self.intensity = intensity
     def model_lambert_lightning(self,normal, light_direction):
         return max(np.dot(normal, light_direction), 0) * self.intensity
+    def model_phong_specular(self, normal, light_direction, view_direction, shininess):
+         
+        reflect_direction = 2 * np.dot(normal, light_direction) * normal - light_direction
+        spec_angle = max(0, np.dot(view_direction, reflect_direction))
+        return self.intensity * (spec_angle ** shininess)
